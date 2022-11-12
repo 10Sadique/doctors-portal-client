a52bcd96a4961cd05f5369ad28a4312bfb203d4c
10Sadique <jafarsjahan@gmail.com>
2022-11-12 18:30:14 +0600

    navbar added

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,29 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const navItems = (
+        <>
+            <li>
+                <Link to={`/`}>Home</Link>
+            </li>
+            <li>
+                <Link to={`/appointment`}>Appointment</Link>
+            </li>
+            <li>
+                <Link to={`/about`}>About</Link>
+            </li>
+            <li>
+                <Link to={`/reviews`}>Reviews</Link>
+            </li>
+            <li>
+                <Link to={`/signin`}>Sign In</Link>
+            </li>
+        </>
+    );
+
     return (
-        <div className="navbar bg-base-100">
+        <div className="max-w-6xl mx-auto navbar bg-base-100">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -25,72 +46,18 @@ const Navbar = () => {
                         tabIndex={0}
                         className="p-2 mt-3 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
                     >
-                        <li>
-                            <a>Item 1</a>
-                        </li>
-                        <li tabIndex={0}>
-                            <a className="justify-between">
-                                Parent
-                                <svg
-                                    className="fill-current"
-                                    xmlns="http://www.w3.org/2000/svg"
-                                    width="24"
-                                    height="24"
-                                    viewBox="0 0 24 24"
-                                >
-                                    <path d="M8.59,16.58L13.17,12L8.59,7.41L10,6L16,12L10,18L8.59,16.58Z" />
-                                </svg>
-                            </a>
-                            <ul className="p-2">
-                                <li>
-                                    <a>Submenu 1</a>
-                                </li>
-                                <li>
-                                    <a>Submenu 2</a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li>
-                            <a>Item 3</a>
-                        </li>
+                        {navItems}
                     </ul>
                 </div>
-                <a className="text-xl normal-case btn btn-ghost">daisyUI</a>
+                <Link
+                    to={`/`}
+                    className="text-xl font-bold normal-case btn btn-ghost"
+                >
+                    Doctors Portal
+                </Link>
             </div>
-            <div className="hidden navbar-center lg:flex">
-                <ul className="p-0 menu menu-horizontal">
-                    <li>
-                        <a>Item 1</a>
-                    </li>
-                    <li tabIndex={0}>
-                        <a>
-                            Parent
-                            <svg
-                                className="fill-current"
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="20"
-                                height="20"
-                                viewBox="0 0 24 24"
-                            >
-                                <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
-                            </svg>
-                        </a>
-                        <ul className="p-2">
-                            <li>
-                                <a>Submenu 1</a>
-                            </li>
-                            <li>
-                                <a>Submenu 2</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a>Item 3</a>
-                    </li>
-                </ul>
-            </div>
-            <div className="navbar-end">
-                <a className="btn">Get started</a>
+            <div className="hidden navbar-end lg:flex">
+                <ul className="p-0 menu menu-horizontal">{navItems}</ul>
             </div>
         </div>
     );
--- src/layout/Main.js
+++ src/layout/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
     return (
         <div>
             <Navbar />
-            <div>
+            <div className="max-w-[360px] md:max-w-3xl lg:max-w-6xl mx-auto my-14">
                 <Outlet />
             </div>
             <Footer />
