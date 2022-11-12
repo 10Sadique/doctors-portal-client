31dd7c33700b384ede4e2a249bbd4025b5f03dd3
10Sadique <jafarsjahan@gmail.com>
2022-11-12 21:14:18 +0600

    footer added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,38 @@
 import React from 'react';
+import footer from '../assets/images/footer.png';
 
 const Footer = () => {
-    return <div>Footer</div>;
+    return (
+        <footer
+            style={{ background: `url(${footer})`, backgroundSize: 'cover' }}
+        >
+            <div className="max-w-6xl p-10 mx-auto footer text-neutral">
+                <div>
+                    <span className="footer-title">Services</span>
+                    <a className="link link-hover">Branding</a>
+                    <a className="link link-hover">Design</a>
+                    <a className="link link-hover">Marketing</a>
+                    <a className="link link-hover">Advertisement</a>
+                </div>
+                <div>
+                    <span className="footer-title">Company</span>
+                    <a className="link link-hover">About us</a>
+                    <a className="link link-hover">Contact</a>
+                    <a className="link link-hover">Jobs</a>
+                    <a className="link link-hover">Press kit</a>
+                </div>
+                <div>
+                    <span className="footer-title">Legal</span>
+                    <a className="link link-hover">Terms of use</a>
+                    <a className="link link-hover">Privacy policy</a>
+                    <a className="link link-hover">Cookie policy</a>
+                </div>
+            </div>
+            <div className="flex items-center justify-center py-10 text-neutral">
+                <p>Copyright © 2022 - All right reserved by Doctors Portal</p>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
