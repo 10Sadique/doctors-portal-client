c0b0e7b7e9787c5cafc172d8a93a304c3a90657b
10Sadique <jafarsjahan@gmail.com>
2022-11-14 16:24:20 +0600

    booking modal form update

--- src/components/BookingModal.js
+++ src/components/BookingModal.js
@@ -1,7 +1,37 @@
 import React from 'react';
 
-const BookingModal = ({ treatment, date }) => {
+const BookingModal = ({ treatment, date, setTreatment }) => {
     const { name, slots } = treatment; // treatment is appointment options
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        const form = e.target;
+
+        const patient = form.name.value;
+        const email = form.email.value;
+        const phone = form.phone.value;
+        const slot = form.slot.value;
+
+        const booking = {
+            appointmentDate: date,
+            treatment: name,
+            patient: patient,
+            email,
+            phone,
+            slot,
+        };
+
+        /**
+         * @TODO send data to the server and
+         * once data is saved then close the modal and
+         * show a toast.
+         */
+        console.log(booking);
+        setTreatment(null);
+        form.reset();
+    };
+
     return (
         <>
             <input
@@ -18,7 +48,7 @@ const BookingModal = ({ treatment, date }) => {
                         ✕
                     </label>
                     <h3 className="text-lg font-bold">{name}</h3>
-                    <form className="mt-5 space-y-3">
+                    <form onSubmit={handleSubmit} className="mt-5 space-y-3">
                         <input
                             type="text"
                             placeholder="Date"
@@ -26,24 +56,32 @@ const BookingModal = ({ treatment, date }) => {
                             value={date}
                             disabled
                         />
-                        <select className="select select-bordered w-full">
+                        <select
+                            name="slot"
+                            className="select select-bordered w-full"
+                        >
                             {slots.map((slot, idx) => (
-                                <option key={idx}>{slot}</option>
+                                <option value={slot} key={idx}>
+                                    {slot}
+                                </option>
                             ))}
                         </select>
                         <input
+                            name="name"
                             type="text"
                             placeholder="Full Name"
                             className="input input-bordered w-full "
                         />
                         <input
+                            name="phone"
                             type="text"
                             placeholder="Phone Number"
                             className="input input-bordered w-full "
                         />
                         <input
+                            name="email"
                             type="email"
-                            placeholder="Email"
+                            placeholder="Email Address"
                             className="input input-bordered w-full "
                         />
                         <button type="submit" className="btn w-full">
