574cbb5ab18e8b38c038192c35ffdb9268cddf02
10Sadique <jafarsjahan@gmail.com>
2022-11-16 20:50:11 +0600

    api version changed

--- src/utils/getOptions.js
+++ src/utils/getOptions.js
@@ -1,5 +1,7 @@
-const getOptions = async () => {
-    const res = await fetch('http://localhost:5000/appointmentOptions');
+const getOptions = async (date) => {
+    const res = await fetch(
+        `http://localhost:5000/v2/appointmentOptions?date=${date}`
+    );
     const data = await res.json();
 
     return data;
