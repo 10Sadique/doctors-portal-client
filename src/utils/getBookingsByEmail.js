fd007772d6f41bbe1fab9e137dcccda5d7dc75a3
10Sadique <jafarsjahan@gmail.com>
2022-11-17 21:15:26 +0600

    sending access token to verify email

--- src/utils/getBookingsByEmail.js
+++ src/utils/getBookingsByEmail.js
@@ -1,5 +1,9 @@
 const getBookingsByEmail = async (email) => {
-    const res = await fetch(`http://localhost:5000/bookings?email=${email}`);
+    const res = await fetch(`http://localhost:5000/bookings?email=${email}`, {
+        headers: {
+            authorization: `bearer ${localStorage.getItem('accessToken')}`,
+        },
+    });
     const data = await res.json();
 
     return data;
